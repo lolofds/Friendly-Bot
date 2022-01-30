aa9d48836455c2ce634147caca1f338827ff52ab
lolofds <94748022+lolofds@users.noreply.github.com>
2022-01-30 23:10:22 +0100

    Add files via upload

---
+++ Friendly bot/main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.event
+async def on_ready():
+    print('bots online')
+
+@bot.command()
+async def sarah(ctx):
+    await ctx.send("Coucou t'as faim ?", file=discord.File('sarah2.png'))
+
+@bot.command()
+async def elon(ctx):
+    await ctx.send(file=discord.File('elon_v2.png'))
+    await ctx.send(file=discord.File('elonito.mp4'))
+
+
+bot.run('OTM3NDA4MjUzMDAyOTE5OTY2.YfbTdw.LEhWuZWQfWtauHFKTt0D_zhlc1A')
